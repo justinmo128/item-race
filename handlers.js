bc148ade4037086ff7a71f33d0d227d71a03c7c2
justinmo128 <j.mo1@share.epsb.ca>
2022-12-13 10:34:31 -0700

    anti-cheat

--- handlers.js
+++ handlers.js
@@ -1,3 +1,7 @@
+let setTimeoutRunning = false;
+let spaceIsHeld = false;
+let spaceIsHeldEvenMore = false;
+
 // Key down handler
 window.addEventListener("keydown", (e) => {
     let keyPressed = e.key;
@@ -5,7 +9,20 @@ window.addEventListener("keydown", (e) => {
         gameState = "gameLoop";
     }
     if (keyPressed === " ") {
-        p1Move = !p1Move;
+        if (!spaceIsHeld) {
+            p1Move = !p1Move;
+        }
+        spaceIsHeld = true;
+        if (!setTimeoutRunning) {
+            setTimeout(() => {
+                if (spaceIsHeld && !spaceIsHeldEvenMore) {
+                    console.log("hi2")
+                    spaceIsHeldEvenMore = true;
+                    setTimeoutRunning = true;
+                    p1Move = !p1Move;
+                }
+            }, 750);
+        }
     }
         
     for (let i = 0; i < 4; i++) {
@@ -29,5 +46,11 @@ window.addEventListener("keyup", (e) => {
         if (keyPressed === rect2keys[i]) {
             rect2move[i] = false;
         }
+    }
+
+    if (e.key === " ") {
+        spaceIsHeld = false;
+        spaceIsHeldEvenMore = false;
+        setTimeoutRunning = false;
     }
 })
